15f18c6feff995af8656aae504fe3d22c036d9dc
dl3c0in <david.lecoin@yahoo.com>
2020-11-17 15:09:03 +0100

    dynamic javascript add image

--- dlecoin/js/index.js
+++ dlecoin/js/index.js
@@ -1,18 +1,53 @@
-const imgLinks = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg";
+let imgLinks = "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg";
 const imgContainer = document.querySelector('#cards');
+const addBtn = document.querySelector('#newImg');
+const removeBtn = document.querySelector('#deleteImg');
+const inputUrl = document.querySelector('#imgurl');
+const alert = document.querySelector('#alert')
+let imgs;
+let imgArray = new Array;
 
 const createImg = () => {
   const newImg = document.createElement('img');
   newImg.src = imgLinks;
   imgContainer.appendChild(newImg);
+  imgs = document.querySelectorAll('img');
+  imgArray = Array.from(imgs);
+  buttonProperties()
 }
 
-const displayImg = (n) => {
-  for (let i = 0; i < n; i++) {
-    createImg()
+const deleteImg = () => {
+  imgArray.pop().remove()
+  buttonProperties()
+}
+
+const isUrl = (url) => {
+  if(url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)){
+    alert.style.display = 'none'
+  imgLinks = url  
+  createImg()
+  }
+  else{
+    alert.style.display = 'block'
   }
 }
 
-window.addEventListener('load', () => {
-  displayImg(9)
-})
+const buttonProperties = () => {
+  if(imgArray.length < 1){
+    removeBtn.disabled = true;
+  }else{
+    removeBtn.disabled = false;
+  }
+}
+
+const init = () => {
+  buttonProperties()
+
+  addBtn.addEventListener('click', () => {
+    isUrl(inputUrl.value)
+  })
+
+  removeBtn.addEventListener('click', deleteImg)
+}
+
+window.addEventListener('load', init)
